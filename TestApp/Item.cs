c4da6b59658203e7d7af07713b07bd8b2f8136a1
Moustafa El-Sawy <mk.elsawy@live.com>
2023-06-20 14:32:59 -0400

    Improvements to Test
    Improvements to Test

---
+++ TestApp/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace TestApp
+{
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public Item(string _name)
+        {
+            Name = _name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- TestApp/MainWindow.xaml.cs
+++ TestApp/MainWindow.xaml.cs
@@ -20,9 +20,62 @@ namespace TestApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Item> Items { get; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Items = GenerateItems();
+            DataContext = this;
+
         }
+
+        private List<Item> GenerateItems()
+        {
+            var items = new List<Item>();
+            for (int i = 1; i <= 20; i++)
+            {
+                var item = new Item($"Item {i}");
+                items.Add(item);
+            }
+
+            for (int i = 1; i <= 20; i++)
+            {
+                var item = new Item($"Apple {i}");
+                items.Add(item);
+            }
+
+            for (int i = 1; i <= 20; i++)
+            {
+                var item = new Item($"Banana {i}");
+                items.Add(item);
+            }
+
+            for (int i = 1; i <= 20; i++)
+            {
+                var item = new Item($"AppleandBanana {i}");
+                items.Add(item);
+            }
+
+            for (int i = 1; i <= 20; i++)
+            {
+                var item = new Item($"Trees {i}");
+                items.Add(item);
+            }
+
+            for (int i = 1; i <= 20; i++)
+            {
+                var item = new Item($"ATrees {i}");
+                items.Add(item);
+            }
+
+            for (int i = 1; i <= 20; i++)
+            {
+                var item = new Item($"ABTrees {i}");
+                items.Add(item);
+            }
+            return items;
+        }
+
     }
 }
