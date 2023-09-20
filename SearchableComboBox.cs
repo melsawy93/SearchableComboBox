04807c663d977e4b312ccf6ad95a992172d9a461
Moustafa El-Sawy <mk.elsawy@live.com>
2023-09-20 18:51:15 -0400

    Update SearchableComboBox.cs

--- SearchableComboBox.cs
+++ SearchableComboBox.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Windows;
 using System.Collections;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Threading;
 using System.Windows.Input;
+using System.Windows.Threading;
 using System.Diagnostics;
 
 namespace SearchableComboBox
@@ -14,10 +14,17 @@ namespace SearchableComboBox
     /// </summary>
     public class SearchableComboBox : ComboBox
     {
-        #region Constructors
+        #region Fields and Constructors
+
         private readonly DispatcherTimer _debounceTimer;
         private bool debug = true;
+        private TextBox _searchTermTextBox;
+        private CollectionViewSource _collectionViewSource;
+        private object _lastSelectedItem;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SearchableComboBox"/> class.
+        /// </summary>
         public SearchableComboBox()
         {
             Debug.WriteLineIf(debug, "Constructor");
@@ -30,26 +37,28 @@ namespace SearchableComboBox
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(SearchableComboBox), new FrameworkPropertyMetadata(typeof(SearchableComboBox)));
         }
-       
-        private TextBox _searchTermTextBox;
+
         public override void OnApplyTemplate()
         {
             Debug.WriteLineIf(debug, "OnApplyTemplate");
             base.OnApplyTemplate();
-
             _searchTermTextBox = Template.FindName("SearchTermTextBox", this) as TextBox;
         }
+
         #endregion
 
         #region CustomSort Support
+
         public static readonly DependencyProperty CustomSortProperty =
             DependencyProperty.Register(
                 "CustomSort",
                 typeof(IComparer),
                 typeof(SearchableComboBox),
-                new PropertyMetadata(null, OnCustomSortChanged)
-            );
+                new PropertyMetadata(null, OnCustomSortChanged));
 
+        /// <summary>
+        /// Gets or sets the custom sort comparer.
+        /// </summary>
         public IComparer CustomSort
         {
             get => (IComparer)GetValue(CustomSortProperty);
@@ -64,37 +73,62 @@ namespace SearchableComboBox
                 lcv.CustomSort = e.NewValue as IComparer;
             }
         }
+
         #endregion
 
         #region IsSearchEnabled
+
         public static readonly DependencyProperty IsSearchEnabledProperty =
             DependencyProperty.Register(
                 "IsSearchEnabled",
                 typeof(bool),
                 typeof(SearchableComboBox),
-                new PropertyMetadata(true)
-            );
+                new PropertyMetadata(true));
 
+        /// <summary>
+        /// Gets or sets a value indicating whether search is enabled.
+        /// </summary>
         public bool IsSearchEnabled
         {
             get => (bool)GetValue(IsSearchEnabledProperty);
             set => SetValue(IsSearchEnabledProperty, value);
         }
+
+        #endregion
+
+        #region IsNullable
+
+        public static readonly DependencyProperty IsNullableProperty =
+            DependencyProperty.Register("IsNullable", typeof(bool), typeof(SearchableComboBox), new PropertyMetadata(false));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the value is nullable.
+        /// </summary>
+        public bool IsNullable
+        {
+            get => (bool)GetValue(IsNullableProperty);
+            set => SetValue(IsNullableProperty, value);
+        }
+
         #endregion
 
         #region Placeholder Text
+
         public static readonly DependencyProperty PlaceholderProperty =
             DependencyProperty.Register("Placeholder", typeof(string), typeof(SearchableComboBox), new PropertyMetadata(string.Empty));
 
+        /// <summary>
+        /// Gets or sets the placeholder text.
+        /// </summary>
         public string Placeholder
         {
-            get { return (string)GetValue(PlaceholderProperty); }
-            set { SetValue(PlaceholderProperty, value); }
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
         }
+
         #endregion
 
         #region OnItemsSourceChanged event
-        private CollectionViewSource _collectionViewSource;
 
         protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
         {
@@ -114,10 +148,10 @@ namespace SearchableComboBox
                 ItemsSource = _collectionViewSource.View;
             }
         }
+
         #endregion
 
         #region Changes only when selected
-        private object _lastSelectedItem;
 
         protected override void OnSelectionChanged(SelectionChangedEventArgs e)
         {
@@ -130,33 +164,32 @@ namespace SearchableComboBox
                 _lastSelectedItem = e.AddedItems[0];
             }
 
-            //if (e.AddedItems.Count <= 0)
-            //{
-            //    e.Handled = true;
-            //}
-            //else
-            //{
-            //    base.OnSelectionChanged(e);
-            //}
             base.OnSelectionChanged(e);
-
             Debug.WriteLineIf(debug, "SelectionBoxItem " + this.SelectionBoxItem);
         }
+
         #endregion
 
         #region Error and border visibility
+
         public static readonly DependencyProperty NotFoundLabelVisibilityProperty =
             DependencyProperty.Register("NotFoundLabelVisibility", typeof(Visibility), typeof(SearchableComboBox), new PropertyMetadata(Visibility.Collapsed));
 
         public static readonly DependencyProperty DropDownBorderVisibilityProperty =
             DependencyProperty.Register("DropDownBorderVisibility", typeof(Visibility), typeof(SearchableComboBox), new PropertyMetadata(Visibility.Visible));
 
+        /// <summary>
+        /// Gets or sets the visibility of the not found label.
+        /// </summary>
         public Visibility NotFoundLabelVisibility
         {
             get => (Visibility)GetValue(NotFoundLabelVisibilityProperty);
             set => SetValue(NotFoundLabelVisibilityProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the visibility of the dropdown border.
+        /// </summary>
         public Visibility DropDownBorderVisibility
         {
             get => (Visibility)GetValue(DropDownBorderVisibilityProperty);
@@ -177,12 +210,17 @@ namespace SearchableComboBox
                 DropDownBorderVisibility = Visibility.Visible;
             }
         }
+
         #endregion
 
         #region Filtering logic
+
         public static readonly DependencyProperty SearchTermProperty =
             DependencyProperty.Register("SearchTerm", typeof(string), typeof(SearchableComboBox), new PropertyMetadata(string.Empty, OnSearchTermChanged));
 
+        /// <summary>
+        /// Gets or sets the search term.
+        /// </summary>
         public string SearchTerm
         {
             get => (string)GetValue(SearchTermProperty);
@@ -218,56 +256,26 @@ namespace SearchableComboBox
                 return true;
             }
 
-            string strItem = item as string;
-            if (strItem == null)
-            {
-                strItem = item.ToString();
-            }
-
+            string strItem = item.ToString();
             return strItem.IndexOf(SearchTerm, StringComparison.OrdinalIgnoreCase) >= 0;
         }
+
+        #endregion
+
+        #region Clear Search
 
         private void ClearSearchTerm(object sender, EventArgs e)
         {
             Debug.WriteLineIf(debug, "ClearSearchTerm");
             SearchTerm = string.Empty;
-
-            // Refresh the view immediately when dropdown closes
             _collectionViewSource.View.Refresh();
-            UpdateVisibility();
 
-            // Restore the last selected item if SelectedItem is null after dropdown closes
-            if (this.SelectedItem == null && _lastSelectedItem != null)
+            if (_lastSelectedItem != null)
             {
-                this.SelectedItem = _lastSelectedItem;
+                SelectedItem = _lastSelectedItem;
             }
         }
-        #endregion
 
-        #region Prevent Focus on Dropdown List
-        protected override void OnPreviewKeyDown(KeyEventArgs e)
-        {
-            Debug.WriteLineIf(debug, "OnPreviewKeyDown");
-            base.OnPreviewKeyDown(e);
-
-            if (e.Key == Key.Down || e.Key == Key.Up || e.Key == Key.Escape)
-            {
-                return;
-            }
-
-            if (_searchTermTextBox != null)
-            {
-                if (!_searchTermTextBox.IsFocused)
-                {
-                    _searchTermTextBox.Focus();
-                }
-            }
-            else
-            {
-                // Handle the event only if the TextBox isn't focused
-                e.Handled = true;
-            }
-        }
         #endregion
     }
 }
